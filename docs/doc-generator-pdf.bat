5ce01085d948778f68fda86970d04582235bf247
Sergio <alu.156502@usj.es>
2026-01-14 20:12:11 +0100

    fix: fixed gitignore

--- docs/doc-generator-pdf.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ..
-call pandoc -s --toc ^
-  --template="C:\.pandoc\templates\easy-pandoc-templates\tex\eisvogel.tex" ^
-  --highlight-style=haddock ^
-  --pdf-engine=xelatex ^
-  -V documentclass=report -V classoption=oneside -V classoption=openany ^
-  -V title="XEngine Documentation" ^
-  README.md -o ".\docs\documentation.pdf"
